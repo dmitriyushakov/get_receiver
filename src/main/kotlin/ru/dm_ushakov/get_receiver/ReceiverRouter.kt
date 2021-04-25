f30169eee6cab88964b5f78ccb523ba6bd0e4726
dmitriyushakov <dmitryushakov1@yandex.ru>
2021-04-25 14:01:41 +1000

    Parse get params and add timestamp

--- src/main/kotlin/ru/dm_ushakov/get_receiver/ReceiverRouter.kt
+++ src/main/kotlin/ru/dm_ushakov/get_receiver/ReceiverRouter.kt
@@ -3,10 +3,24 @@ package ru.dm_ushakov.get_receiver
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.model.rest.RestBindingMode
 import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Configuration
 class ReceiverRouter:RouteBuilder() {
     private val messages = mutableListOf<Any>()
+    private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS") as DateTimeFormatter
+
+    private fun getCurrentDatetime():String {
+        val dateTime = LocalDateTime.now()
+        return dateTimeFormatter.format(dateTime)
+    }
+
+    private fun parseGetParams(encoded:String):Map<String,Any> =
+        encoded
+            .split('&')
+            .map { it.split('=') }
+            .associate { it[0] to it[1] }
 
     override fun configure() {
         restConfiguration().apply {
@@ -20,7 +34,9 @@ class ReceiverRouter:RouteBuilder() {
 
         rest().apply {
             get("/receive").route().apply {
-                setBody().mvel("request.headers")
+                setBody().mvel("request.headers.CamelHttpQuery")
+                setHeader("datetime", this@ReceiverRouter::getCurrentDatetime)
+                setBody().message { parseGetParams(it.body as String) + mapOf("datetime" to it.getHeader("datetime")) }
                 process { messages.add( it.`in`.body ) }
             }.endRest()
 
